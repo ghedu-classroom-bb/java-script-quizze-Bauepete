cf8972a36fefdd4b5fb7612d3e66994d652895ea
Peter Bauer <p.bauer@htl-leonding.ac.at>
2018-10-09 11:25:41 +0200

    Removed comments

--- math.js
+++ math.js
@@ -1,10 +1,8 @@
 exports.add = function (x, y) {
-    // ToDo: Add the necessary code to add two numbers
     return x + y;
 }
 
 exports.sub = function (x, y) {
-    // ToDo: Add the necessary code to subtract two numbers
     return x - y;
 }
 
